10de0f420746e137bd0f339302d3b2d05a61cf6b
pandupan <pangestup60@gmail.com>
2023-06-16 14:28:28 +0700

    certificate done

--- components/builder/Form/FormCertificate.tsx
+++ components/builder/Form/FormCertificate.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import InputDate from "@/components/form/InputDate";
 import InputText from "@/components/form/InputText";
 import InputTextArea from "@/components/form/InputTextArea";
@@ -8,50 +8,124 @@ import { useAwards, IAwardItem } from "@/stores/awards";
 function FormCertificate() {
   const { values: dataBasic, reset } = useBasicDetails();
   const { awards, updateAward } = useAwards();
-  const awardItem = awards[0];
+  const [certificateCount, setCertificateCount] = useState(1);
 
-  const handleOnChangeInput = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleOnChangeInput = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, index: number) => {
     const { name, value } = e.target;
 
     const updatedAwardItem: IAwardItem = {
-      ...awardItem,
+      ...awards[index],
       [name]: value,
     };
 
-    updateAward(0, updatedAwardItem);
+    updateAward(index, updatedAwardItem);
   };
 
-  return (
-    <form action="" className="mt-[15px]">
-      <div className="flex flex-col lg:flex-row gap-4">
-        <div className="lg:w-1/2">
-          <InputText
-            id="title"
-            label="Title"
-            placeholder="e.g. Certified Data Analyst"
-            value={awardItem.title}
-            onChange={handleOnChangeInput}
-          />
-        </div>
-        <div className="lg:w-1/2">
-          <InputDate
-            id="date"
-            label="Certified Date"
-            value={awardItem.date || ""}
-            onChange={handleOnChangeInput}
-          />
-        </div>
-      </div>
+  const handleAddCertificate = () => {
+    setCertificateCount(certificateCount + 1);
+  };
 
-      <div className="mt-4">
-        <InputTextArea
-          id="summary"
-          label="Description"
-          value={awardItem.summary}
-          onChange={handleOnChangeInput}
-        />
+  const handleRemoveCertificate = () => {
+    if (certificateCount > 1) {
+      setCertificateCount(certificateCount - 1);
+    }
+  };
+
+  const handleResetCertificates = () => {
+    setCertificateCount(1);
+  };
+
+  const certificateForms = [];
+
+  for (let i = 0; i < certificateCount; i++) {
+    const awardItem = awards[i];
+
+    certificateForms.push(
+      <div key={i} className="mt-[15px]">
+        <div className="flex flex-col lg:flex-row gap-4">
+          <div className="lg:w-1/2">
+            <InputText
+              id={`title-${i}`}
+              label="Title"
+              placeholder="e.g. Certified Data Analyst"
+              value={awardItem.title}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleOnChangeInput(e, i)}
+              name="title"
+            />
+          </div>
+          <div className="lg:w-1/2">
+            <InputDate
+              id={`date-${i}`}
+              label="Certified Date"
+              value={awardItem.date || ""}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleOnChangeInput(e, i)}
+              name="date"
+            />
+          </div>
+          <div className="lg:w-1/2">
+            <InputText
+              id={`date-${i}`}
+              label="Awarder"
+              value={awardItem.awarder || ""}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleOnChangeInput(e, i)}
+              name="awarder"
+            />
+          </div>
+          <div className="lg:w-1/2">
+            <InputDate
+              id={`date-${i}`}
+              label="Certified Date"
+              value={awardItem.date || ""}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => handleOnChangeInput(e, i)}
+              name="date"
+            />
+          </div>
+
+
+        </div>
+
+        <div className="mt-4">
+          <InputTextArea
+            id={`summary-${i}`}
+            label="Description"
+            value={awardItem.summary}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) => handleOnChangeInput(e, i)}
+            name="summary"
+          />
+        </div>
       </div>
-    </form>
+    );
+  }
+
+  return (
+    <div>
+      {certificateForms}
+      <div className="flex items-center mt-4 justify-end gap-5">
+        {certificateCount > 1 && (
+          <button
+            type="button"
+            className="px-4 py-2 text-sm font-medium text-red-500 border border-tertiary rounded hover:bg-red-500 hover:text-white hover:border-red-500"
+            onClick={handleRemoveCertificate}
+          >
+            -
+          </button>
+        )}
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium text-white bg-sky-500 rounded hover:bg-sky-600"
+          onClick={handleResetCertificates}
+        >
+          Reset
+        </button>
+        <button
+          type="button"
+          className="px-4 py-2 text-sm font-medium text-red-500 border border-tertiary rounded hover:bg-red-500 hover:text-white hover:border-red-500"
+          onClick={handleAddCertificate}
+        >
+          +
+        </button>
+      </div>
+    </div>
   );
 }
 
